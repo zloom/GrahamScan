1b85ad4d21ee41e10dc2ae7932b9f22d956ddbb3
Anton Zalyaldinov <a.zalyaldinov@systtech.ru>
2016-12-13 19:49:33 +0200

    fix: undo main

--- GrahamScan/GrahamScan/Program.cs
+++ GrahamScan/GrahamScan/Program.cs
@@ -12,79 +12,37 @@ namespace GrahamScan
 {
     public static class Program
     {
-        //static void Main(string[] args)
-        //{
-        //    var input = File.ReadLines("input.txt")
-        //        .Select(p => p.Split(','))
-        //        .Select(p => new Point(int.Parse(p[0]), int.Parse(p[1])))
-        //        .ToArray();
-
-        //    var output = GrahamScan.Process(input);
-
-        //    File.WriteAllLines("output.txt", output.Select(p => String.Format("{0},{1}", p.X, p.Y)));
-
-        //    var width = input.Max(p => p.X) + 100;
-        //    var height = input.Max(p => p.Y) + 100;
-
-        //    using (var bitmap = new Bitmap(width, height))
-        //    {
-        //        using (var g = Graphics.FromImage(bitmap))
-        //        {
-        //            g.FillRectangle(Brushes.White, new Rectangle(0, 0, width, height));
-        //            g.DrawLines(Pens.Red, output.Select(p => new Point(p.X + 50, p.Y + 50)).ToArray());
-        //        }
-
-        //        foreach (var pixel in input)
-        //        {
-        //            bitmap.SetPixel(pixel.X + 50, pixel.Y + 50, Color.Green);
-        //        }
-
-        //        bitmap.Save("output.bmp");
-        //    }
-
-        //}
-
-
         static void Main(string[] args)
         {
-            var rnd = new Random();
+            var input = File.ReadLines("input.txt")
+                .Select(p => p.Split(','))
+                .Select(p => new Point(int.Parse(p[0]), int.Parse(p[1])))
+                .ToArray();
 
-            var i = 0;
-            while (true)
+            var output = GrahamScan.Process(input);
+
+            File.WriteAllLines("output.txt", output.Select(p => String.Format("{0},{1}", p.X, p.Y)));
+
+            var width = input.Max(p => p.X) + 100;
+            var height = input.Max(p => p.Y) + 100;
+
+            using (var bitmap = new Bitmap(width, height))
             {
-                i++;
-                var data = Enumerable.Range(0, 4)
-                    .Select(p => new Point(rnd.Next(0, 10), rnd.Next(0, 10)))
-                    .ToArray();               
-
-                try
+                using (var g = Graphics.FromImage(bitmap))
                 {
-                    GrahamScan.Process(data);
+                    g.FillRectangle(Brushes.White, new Rectangle(0, 0, width, height));
+                    g.DrawLines(Pens.Red, output.Select(p => new Point(p.X + 50, p.Y + 50)).ToArray());
                 }
-                catch (Exception ex)
+
+                foreach (var pixel in input)
                 {
-
-                    var width = data.Max(p => p.X) + 100;
-                    var height = data.Max(p => p.Y) + 100;
-
-                    using (var bitmap = new Bitmap(width, height))
-                    {
-                        using (var g = Graphics.FromImage(bitmap))
-                        {
-                            g.FillRectangle(Brushes.White, new Rectangle(0, 0, width, height));                           
-                        }
-
-                        foreach (var pixel in data)
-                        {
-                            bitmap.SetPixel(pixel.X + 50, pixel.Y + 50, Color.Green);
-                        }
-
-                        bitmap.Save(i + "-output.bmp");
-                    }
-                   
+                    bitmap.SetPixel(pixel.X + 50, pixel.Y + 50, Color.Green);
                 }
+
+                bitmap.Save("output.bmp");
             }
 
         }
+      
     }
 }
