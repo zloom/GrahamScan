99ca0373febd9ea6971dfed0c8a023a48eba6e7c
Anton Zalyaldinov <a.zalyaldinov@systtech.ru>
2016-12-13 18:13:48 +0200

    initial

---
+++ GrahamScan/GrahamScan/GrahamScan.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrahamScan
+{
+    public static class StackExtensions
+    {
+        public static Point NextToTop(this Stack<Point> source)
+        {
+            var top = source.Pop();
+            var next = source.Peek();
+            source.Push(top);
+            return next;
+        }
+    }
+
+    public static class PointExtensions
+    {
+        public static int Orientation(this Point root, Point q, Point r)
+        {
+            int val = (q.Y - root.Y) * (r.X - q.X) - (q.X - root.X) * (r.Y - q.Y);
+            if (val == 0) return 0;
+            return (val > 0) ? 1 : 2;
+        }
+
+        public static int DistanceTo(this Point source, Point target)
+        {
+            return (source.X - target.X) * (source.X - target.X) + (source.Y - target.Y) * (source.Y - target.Y);
+        }
+    }
+
+    public class RootPointComparer : Comparer<Point>
+    {
+        private readonly Point Root;
+        public RootPointComparer(Point root)
+        {
+            Root = root;
+        }
+
+        public override int Compare(Point x, Point y)
+        {
+            if (x == y)
+            {
+                return 0;
+            }
+
+            int o = Root.Orientation(x, y);
+            if (o == 0)
+                return (Root.DistanceTo(y) >= Root.DistanceTo(x)) ? -1 : 1;
+
+            return (o == 2) ? -1 : 1;
+        }
+    }
+
+    public static class GrahamScan
+    {
+        public static Stack<Point> Process(Point[] points)
+        {
+            if (points.Length < 3)
+            {
+                throw new Exception("Not enought points for convex hull");
+            }
+
+            var sorted = points.OrderBy(p => p.Y).ThenBy(p => p.X).ToArray();
+            var leftDown = sorted.First();
+            sorted = sorted.Skip(1).OrderBy(p => p, new RootPointComparer(leftDown)).ToArray();
+            points = new[] { leftDown }.Concat(sorted).ToArray();
+
+            var stack = new Stack<Point>();
+            stack.Push(points[0]);
+            stack.Push(points[1]);
+            stack.Push(points[2]);
+
+            for (int i = 3; i < points.Length; i++)
+            {
+                while (stack.NextToTop().Orientation(stack.Peek(), points[i]) != 2)
+                {
+                    stack.Pop();
+                }
+                stack.Push(points[i]);
+            }
+
+            return stack;
+        }
+    }
+
+}
---
+++ GrahamScan/GrahamScan/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrahamScan
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadLines("input.txt")
+                .Select(p => p.Split(','))
+                .Select(p => new Point(int.Parse(p[0]), int.Parse(p[1])))
+                .ToArray();
+
+            var output = GrahamScan.Process(input);
+
+            File.WriteAllLines("output.txt", output.Select(p => String.Format("{0},{1}", p.X, p.Y)));
+
+            var width = input.Max(p => p.X) + 100;
+            var height = input.Max(p => p.Y) + 100;
+
+            using (var bitmap = new Bitmap(width, height))
+            {
+                using (var g = Graphics.FromImage(bitmap))
+                {
+                    g.FillRectangle(Brushes.White, new Rectangle(0, 0, width, height));
+                    g.DrawLines(Pens.Red, output.Select(p => new Point(p.X + 50, p.Y + 50)).ToArray());
+                }
+
+                foreach (var pixel in input)
+                {
+                    bitmap.SetPixel(pixel.X + 50, pixel.Y + 50, Color.Green);
+                }
+
+                bitmap.Save("output.bmp");
+            }
+
+        }
+    }
+}
