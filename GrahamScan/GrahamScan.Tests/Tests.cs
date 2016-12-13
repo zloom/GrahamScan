bbb9f63ff148be87dd86ed83eacb6108a9bab6a0
Anton Zalyaldinov <a.zalyaldinov@systtech.ru>
2016-12-13 19:47:03 +0200

    test: line points test

---
+++ GrahamScan/GrahamScan.Tests/Tests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrahamScan.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void GrahamScan_LinePoints_ShouldNotThrowEmptyStackException()
+        {
+            //Arrange
+            var data = new Point[] { new Point(1, 1), new Point(2, 2), new Point(3, 3), new Point(4, 4) };
+
+            //Act
+            var result = GrahamScan.Process(data);
+
+            //Assert
+            Assert.IsTrue(true);
+        }
+    }
+}
